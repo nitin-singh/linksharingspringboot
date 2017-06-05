707e32845b8ee9f1d783a9791b922d5544935d88
Nitin Kumar Singh <nitin.kumar@tothenew.com>
2017-06-05 14:43:04 +0530

    initialized project with spring initializer and added git ignore file

---
+++ src/main/java/com/ls/LinksharingspringbootApplication.java
@@ -0,0 +1,12 @@
+package com.ls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinksharingspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinksharingspringbootApplication.class, args);
+	}
+}
